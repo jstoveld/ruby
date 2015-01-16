a8299d5d85a1afc45973326719ce749c2819cde2
jstoveld <jstoveld@gmail.com>
2015-01-16 16:42:20 -0500

    Updates to testing. Cleaned up ruby folder by making a start folder.

---
+++ testing/2-spec.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'minitest/spec'
+
+describe 'MyTests' do 
+before do 
+	puts "Tests are beginning"
+end
+
+
+	it "adds 2 + 2" do 
+		(2+2).must_equal 4
+	end
+	it "includes a frog" do 
+		%w(dog cat frog).must_include('frog')
+	end
+
+	it "must be a fixnum" do 
+		(2+2).must_be_instance_of Fixnum
+	end
+
+	it "shoukd raise an error" do 
+		array = []
+		lambda { array.hello }.must_raise NoMethodError
+	end
+end
